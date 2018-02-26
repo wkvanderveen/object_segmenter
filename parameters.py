7fde2fd4f66643de4cc6c4f484ad7d374eddc62d
wkvanderveen <w.k.van.der.veen.2@student.rug.nl>
2018-02-27 00:15:58 +0100

    Make system use greyscale instead of RBG input. Display convolutional layers. Many bugfixes.

--- parameters.py
+++ parameters.py
@@ -1,27 +1,83 @@
 import os
+import math
 from shutil import rmtree
 
 overwrite_existing_model = True
 predict = True
 model_dir = './model_data/'
-img_width = 56
-img_height = 56
-img_channels = 3
-seg_channels = 1
+img_width = 32
+img_height = 32
 train_percentage = 80
-max_img = 200
-batch_size = 8
-num_epochs_train = 200
+max_img = 64
+batch_size = 5
+num_epochs_train = 100
 num_epochs_eval = 5
-steps = 500
+steps = 300
 learning_rate = 0.01
-num_hidden = 128
+num_hidden = 64
 num_filters = 8
-dropout_rate = 0.1
+filter_size = [5, 5]
+dropout_rate = 0.4
 seg_threshold = 0.5
+
+
+plot_layers = {
+    "Any":      False,
+    "Conv1":    True,
+    "Dilated":  True,
+    "Conv2":    True,
+    "Deconv":   True
+}
 
 
 def rem_existing_model():
     """Delete map containing model metadata."""
     if overwrite_existing_model and os.path.exists(model_dir):
         rmtree(model_dir)
+
+
+def empty_dir(path):
+    """
+    Delete all files and folders in a directory
+    :param path: string, path to directory
+    :return: nothing
+    """
+    for the_file in os.listdir(path):
+        file_path = os.path.join(path, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                rmtree(file_path)
+        except Exception as e:
+            print('Warning: {}'.format(e))
+
+
+def get_grid_dim(x):
+    """
+    Transforms x into product of two integers
+    :param x: int
+    :return: two ints
+    """
+    factors = prime_powers(x)
+    if len(factors) % 2 == 0:
+        i = int(len(factors) / 2)
+        return factors[i], factors[i - 1]
+
+    i = len(factors) // 2
+    return factors[i], factors[i]
+
+
+def prime_powers(n):
+    """
+    Compute the factors of a positive integer
+    Algorithm from https://rosettacode.org/wiki/Factors_of_an_integer#Python
+    :param n: int
+    :return: set
+    """
+    factors = set()
+    for x in range(1, int(math.sqrt(n)) + 1):
+        if n % x == 0:
+            factors.add(int(x))
+            factors.add(int(n // x))
+    return sorted(factors)
--- tumor_detector.py
+++ tumor_detector.py
@@ -16,11 +16,42 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'    # suppress warnings
 tf.logging.set_verbosity(tf.logging.INFO)   # display tensorflow info
 
 
+def plot_conv(filters):
+    n_filters = filters.shape[3]
+
+    grid_r, grid_c = par.get_grid_dim(n_filters)
+
+    v_min = np.min(filters)
+    v_max = np.max(filters)
+
+    # Instantiate figure for this channel
+    fig, axes = plt.subplots(min([grid_r, grid_c]),
+                             max([grid_r, grid_c]))
+
+    fig.suptitle("Convolutional filters")
+    fig.text(0, 0, "number of filters: " + str(n_filters) +
+                   "\nfilter size: " + str(filters.shape[0]) +
+                   "x" + str(filters.shape[1]))
+
+    # Iterate over subplots and plot image of filter or convolution
+    for l, ax in enumerate(axes.flat):
+
+        img = filters[:, :, 0, l]
+        ax.imshow(img,
+                  vmin=v_min,
+                  vmax=v_max,
+                  interpolation='nearest',
+                  cmap='seismic')
+
+        # Remove axis labels
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    plt.show()
+
+
 def display_prediction(imgs):
     """Display input, prediction, and label images."""
-
-    # Set color maps for the input, prediction, and label images.
-    cmaps = [None, 'gray', 'gray']
 
     # Initialize figure
     fig = plt.figure()
@@ -29,7 +60,7 @@ def display_prediction(imgs):
     for img in range(3):
         sub = fig.add_subplot(1, 3, img+1)
         sub.set_title(['Input', 'Prediction', 'Label'][img])
-        plt.imshow(imgs[img], cmap=cmaps[img])
+        plt.imshow(imgs[img], cmap='gray')
         plt.axis('off')
     plt.show()
 
@@ -63,32 +94,38 @@ def read_images():
         if not os.path.isfile(img_dir + filename + '.jpg'):
             continue
 
-        print(f"Reading images... Now at {100*n_files/par.max_img:.2f}%",
-              end='\r',
-              flush=True)
+        #print(f"Reading images... Now at {100*n_files/par.max_img:.2f}%",
+        #      end='\r',
+        #      flush=True)
 
         n_files += 1
 
         # Read the images from their files
+        current_img = cv2.imread(img_dir + filename + '.jpg')
+
         current_seg = cv2.imread(seg_dir + seg_file)
 
-        current_img = cv2.cvtColor(cv2.imread(img_dir + filename + '.jpg'),
-                                   cv2.COLOR_BGR2RGB)
-
         # Resize the images to the standard size (from the parameters)
-        resized_seg = cv2.resize(
-            src=current_seg,
-            dsize=(par.img_width, par.img_height))
         resized_img = cv2.resize(
             src=current_img,
-            dsize=(par.img_width, par.img_height))
+            dsize=(par.img_width, par.img_height),
+            interpolation=cv2.INTER_NEAREST)
+        resized_seg = cv2.resize(
+            src=current_seg,
+            dsize=(par.img_width, par.img_height),
+            interpolation=cv2.INTER_NEAREST)
+
+        # Convert BGR to greyscale color format
+        recolored_img = cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+        recolored_img = recolored_img.astype(float)
 
         # Convert segmentation image to binary color format.
-        resized_seg = np.ceil(np.mean(resized_seg, axis=2))
+        recolored_seg = np.ceil(np.amax(resized_seg, axis=2))
+        recolored_seg = recolored_seg.astype(int)
 
         # Append resized images to their respective lists.
-        seg.append(resized_seg)
-        img.append(resized_img)
+        img.append(recolored_img)
+        seg.append(recolored_seg)
 
     print("\nReading images completed!\n")
 
@@ -98,8 +135,7 @@ def read_images():
 
     train_img = np.zeros(shape=(n_train,
                                 par.img_width,
-                                par.img_height,
-                                par.img_channels),
+                                par.img_height),
                          dtype=np.float32)
     train_seg = np.zeros(shape=(n_train,
                                 par.img_width,
@@ -107,8 +143,7 @@ def read_images():
                          dtype=np.float32)
     test_img = np.zeros(shape=(n_test,
                                par.img_width,
-                               par.img_height,
-                               par.img_channels),
+                               par.img_height),
                         dtype=np.float32)
     test_seg = np.zeros(shape=(n_test,
                                par.img_width,
@@ -121,19 +156,19 @@ def read_images():
     # Randomly bifurcate the input and segmentation images into  a train
     # and test set.
     for rand_idx in rand.sample(range(n_files), n_files):
-        print(f"Sampling images... Now at {100*(c_train+c_test)/n_files:.2f}%",
-              end='\r',
-              flush=True)
+        # print(f"Sampling images... Now at {100*(c_train+c_test)/n_files:.2f}%",
+        #       end='\r',
+        #       flush=True)
 
         # Store images in train or test set, depending on whether the
         # limit for the training images is reached.
         if c_train < n_train:
-            train_img[c_train] = img[rand_idx]
-            train_seg[c_train] = seg[rand_idx]
+            train_img[c_train] = img[rand_idx] / 256
+            train_seg[c_train] = seg[rand_idx] / 256
             c_train += 1
         else:
-            test_img[c_test] = img[rand_idx]
-            test_seg[c_test] = seg[rand_idx]
+            test_img[c_test] = img[rand_idx] / 256
+            test_seg[c_test] = seg[rand_idx] / 256
             c_test += 1
 
     return train_img, train_seg, test_img, test_seg
@@ -144,14 +179,15 @@ def model_fn(features, labels, mode):
 
     # Input layer
     input_layer = tf.reshape(
-        features["x"], [-1, par.img_width, par.img_height, par.img_channels])
+        features["x"], [-1, par.img_width, par.img_height, 1])
 
     # Down block
     conv1 = tf.layers.conv2d(name="Conv1_layer",
                              inputs=input_layer,
                              filters=par.num_filters,
-                             kernel_size=[5, 5],
-                             padding="same")
+                             kernel_size=par.filter_size,
+                             padding="same",
+                             bias_initializer=tf.random_normal_initializer)
 
     conv_norm1 = tf.layers.batch_normalization(inputs=conv1,
                                                name="Batch_normalization1")
@@ -159,23 +195,25 @@ def model_fn(features, labels, mode):
     dense1 = tf.layers.dense(inputs=conv_norm1,
                              units=par.num_hidden,
                              activation=tf.nn.relu,
-                             name="Dense1")
-
+                             name="Dense1",
+                             bias_initializer=tf.random_normal_initializer)
 
     dil = tf.layers.conv2d(name="Dilation",
                            inputs=dense1,
                            filters=par.num_filters,
-                           kernel_size=[5, 5],
+                           kernel_size=par.filter_size,
                            padding="same",
-                           dilation_rate=(2, 2))
+                           dilation_rate=(2, 2),
+                           bias_initializer=tf.random_normal_initializer)
 
     #################################################################
     # Up block
     conv2 = tf.layers.conv2d(name="Conv2_layer",
                              inputs=dil,
                              filters=par.num_filters,
-                             kernel_size=[5, 5],
-                             padding="same")
+                             kernel_size=par.filter_size,
+                             padding="same",
+                             bias_initializer=tf.random_normal_initializer)
 
     conv_norm2 = tf.layers.batch_normalization(inputs=conv2,
                                                name="Batch_normalization2")
@@ -187,31 +225,29 @@ def model_fn(features, labels, mode):
 
     deconv = tf.layers.conv2d_transpose(inputs=dense2,
                                         filters=par.num_filters,
-                                        kernel_size=[5, 5],
+                                        kernel_size=par.filter_size,
                                         padding="same",
                                         name="Deconv")
-    print(f"\n\nAfter deconv shape is {deconv.shape}.\n\n")
 
     # Output images
 
-    dense = tf.layers.dense(inputs=deconv,
-                            units=1,
-                            activation=tf.nn.relu,
-                            name="Final_dense")
-    print(f"\n\nAfter dense shape is {dense.shape}.\n\n")
+    dense3 = tf.layers.dense(inputs=deconv,
+                             units=1,
+                             activation=tf.nn.relu,
+                             name="Dense3",
+                             bias_initializer=tf.random_normal_initializer)
 
-    output = tf.reshape(dense,
+    output = tf.reshape(dense3,
                         [-1, par.img_width, par.img_height])
 
-    print(f"\n\nAfter output shape is {output.shape}.\n\n")
     # Save the output (for PREDICT mode)
     predictions = {"seg_out": output}
     if mode == tf.estimator.ModeKeys.PREDICT:
         return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
     # Calculate loss (for both TRAIN and EVAL modes)
-    loss = tf.losses.absolute_difference(labels=labels,
-                                         predictions=output)
+    loss = tf.losses.mean_squared_error(labels=labels,
+                                        predictions=output)
 
     # Configure the training op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
@@ -267,7 +303,7 @@ def main(config):
         x={"x": test_img},
         y=test_seg,
         num_epochs=par.num_epochs_eval,
-        shuffle=False)
+        shuffle=True)
 
     print(tumor_detector.evaluate(input_fn=eval_input_fn))
 
@@ -279,15 +315,25 @@ def main(config):
     if par.predict:
         single_image = test_img[:1]
 
-        plot_input_fn = tf.estimator.inputs.numpy_input_fn(
+        pred_input_fn = tf.estimator.inputs.numpy_input_fn(
             x={"x": single_image},
             batch_size=1,
             shuffle=False)
 
-        for single_predict in tumor_detector.predict(plot_input_fn):
-            display_prediction([single_image[0, :, :, :],
+        for single_predict in tumor_detector.predict(pred_input_fn):
+            display_prediction([test_img[0, :, :],
                                 single_predict["seg_out"],
                                 test_seg[0, :, :]])
+
+    if par.plot_layers["Any"]:
+        if par.plot_layers["Conv1"]:
+            plot_conv(tumor_detector.get_variable_value("Conv1_layer/kernel"))
+        if par.plot_layers["Dilated"]:
+            plot_conv(tumor_detector.get_variable_value("Dilation/kernel"))
+        if par.plot_layers["Conv2"]:
+            plot_conv(tumor_detector.get_variable_value("Conv2_layer/kernel"))
+        if par.plot_layers["Deconv"]:
+            plot_conv(tumor_detector.get_variable_value("Deconv/kernel"))
 
 
 if __name__ == "__main__":
